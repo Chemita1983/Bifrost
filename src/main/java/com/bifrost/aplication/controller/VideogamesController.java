b97f1f2b84bfac55893a1cda6b1ba17c2192ef66
chema <josemjimenezvega@gmail.com>
2019-07-03 20:40:20 +0200

    feat: update html

--- src/main/java/com/bifrost/aplication/controller/VideogamesController.java
+++ src/main/java/com/bifrost/aplication/controller/VideogamesController.java
@@ -4,26 +4,24 @@ import com.bifrost.aplication.domain.OutVideogame;
 import com.bifrost.aplication.service.impl.VideogameServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class VideogamesController {
 
     @Autowired
-    private VideogameServiceImpl videogameService = null;
+    private VideogameServiceImpl videogameService;
 
-
- /* Change @Controller
     @GetMapping(value="/getVideogamesWeb" ,produces = MediaType.APPLICATION_JSON_VALUE)
     public String getVideogamesWeb(Model model) {
-        model.addAttribute("outvideogames",videogameService.getAllVideogames().toCompletableFuture().join());
+        model.addAttribute("outvideogames", videogameService.getAllVideogames().toCompletableFuture().join());
         return "index";
-    }*/
+    }
 
     @GetMapping(value="/getVideogame/{name}" ,produces = MediaType.APPLICATION_JSON_VALUE)
     public List<OutVideogame> getVideogame(@PathVariable("name") String gameName) {
