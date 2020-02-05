d7a7f1ec54a15c4b569380efd91516da113a0670
chema <josemjimenezvega@gmail.com>
2020-02-05 01:38:28 +0100

    update springboot version and change import

--- src/main/java/com/bifrost/aplication/controller/BifrostExceptionController.java
+++ src/main/java/com/bifrost/aplication/controller/BifrostExceptionController.java
@@ -3,12 +3,13 @@ package com.bifrost.aplication.controller;
 import com.bifrost.aplication.exceptions.BadRequestException;
 import com.bifrost.aplication.exceptions.ExceptionResponse;
 import com.bifrost.aplication.exceptions.NotFoundException;
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @ControllerAdvice
@@ -17,8 +18,8 @@ public class BifrostExceptionController{
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public @ResponseBody
-    ExceptionResponse handleResourceNotFound(final NotFoundException exception,
-                                             final HttpServletRequest request) {
+    ExceptionResponse handleResourceNotFound(NotFoundException exception,
+                                             HttpServletRequest request) {
 
         ExceptionResponse error = new ExceptionResponse();
         error.setErrorMessage(exception.getMessage());
@@ -29,8 +30,8 @@ public class BifrostExceptionController{
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-    public @ResponseBody ExceptionResponse handleException(final Exception exception,
-                                                           final HttpServletRequest request) {
+    public @ResponseBody ExceptionResponse handleException(Exception exception,
+                                                           HttpServletRequest request) {
 
         ExceptionResponse error = new ExceptionResponse();
         error.setErrorMessage(exception.getMessage());
@@ -42,8 +43,8 @@ public class BifrostExceptionController{
 
     @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public @ResponseBody ExceptionResponse handleBadRequestException(final Exception exception,
-                                                           final HttpServletRequest request) {
+    public @ResponseBody ExceptionResponse handleBadRequestException(Exception exception,
+                                                           HttpServletRequest request) {
 
         ExceptionResponse error = new ExceptionResponse();
         error.setErrorMessage(exception.getMessage());
